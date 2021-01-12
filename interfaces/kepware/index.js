75776b2e1c8f98d3033366b588585175ebaa4993
Daniel Dangond <ddangond@ptc.com>
2021-01-12 10:14:58 -0500

    Merging from ddgond-ptc repo

--- interfaces/kepware/index.js
+++ interfaces/kepware/index.js
@@ -6,266 +6,100 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+const Kepware = require('./kepware');
 var server = require('../../../../libraries/hardwareInterfaces');
 var settings = server.loadHardwareInterface(__dirname);
+var kepware;
+var itemCounts = {};
 
 exports.enabled = settings('enabled');
 exports.configurable = true; // can be turned on/off/adjusted from the web frontend
 
 if (exports.enabled) {
-    const fetch = require('node-fetch');
+    const onItemAdd = (item, permissions) => {
+        const parsedId = Kepware.parseId(item.nodeId);
+        const objectName = `${settings('name')}_${parsedId.deviceName}`;
+        const frameName = parsedId.groupName;
+        const nodeName = parsedId.tagName;
+        if (permissions.canRead) {
+            server.addNode(objectName, frameName, `read${nodeName}`, 'node', {x:50,y:itemCounts[objectName]*100});
+        }
+        if (permissions.canWrite) {
+            server.addNode(objectName, frameName, `write${nodeName}`, 'node', {x:-50,y:itemCounts[objectName]*100});
+            server.addReadListener(objectName, frameName, `write${nodeName}`, data => {
+                // console.log(`Writing data for ${item.browseName.name} = ${data.value}`);
+                kepware.writeItem(item, data.value);
+            });
+        }
+        if (!itemCounts[objectName]) {
+            itemCounts[objectName] = 0;
+        }
+        itemCounts[objectName]++;
+        // console.log(`Added item ${item.browseName.name}`);
+    }
 
-    let kepware1 = null;
+    const onItemUpdate = (item, result) => {
+        const parsedId = Kepware.parseId(item.nodeId);
+        const objectName = `${settings('name')}.${parsedId.deviceName}`;
+        const frameName = parsedId.groupName;
+        const nodeName = parsedId.tagName;
+        server.write(objectName, frameName, `read${nodeName}`, Number(result.value.value));
+        // console.log(`Monitored item update for ${nodeId.slice(0, nodeId.lastIndexOf('.'))} ${item.browseName.name}: ${result.value.value}`);
+    }
+
+    const onItemRemove = (item) => {
+        const parsedId = Kepware.parseId(item.nodeId);
+        const objectName = `${settings('name')}.${parsedId.deviceName}`;
+        const frameName = parsedId.groupName;
+        const nodeName = parsedId.tagName;
+        server.removeNode(objectName, frameName, `read${nodeName}`);
+        server.removeNode(objectName, frameName, `write${nodeName}`);
+        // console.log(`Removed item ${item.browseName.name}`);
+    }
 
     server.addEventListener('reset', function() {
-        console.log('reset kepware');
-        kepware1 = null;
+        console.log('Resetting Kepware interface');
+        if (kepware) {
+            kepware.disconnect();
+        }
+        itemCounts = {};
         setup();
     });
 
     function setup() { // eslint-disable-line no-inner-declarations
-        console.log('setup kepware');
         settings = server.loadHardwareInterface(__dirname);
-
+        
         /**
          * These settings will be exposed to the webFrontend to potentially be modified
          */
         exports.settings = {
-            ip: {
-                value: settings('ip'),
+            discoveryUrl: {
+                value: settings('discoveryUrl', ''), // "opc.tcp://ubuntuPtc.local:49330"
                 type: 'text',
-                helpText: 'The IP address of the KEPServerEX you want to connect to.'
+                helpText: 'The url and port (default: 49330) of the Kepware OPC UA discovery endpoint you want to connect to.'
             },
-            port: {
-                value: settings('port'),
-                type: 'number',
-                default: 39320,
-                helpText: 'The port of the IoT Gateway on the KEPServerEx.'
+            username: {
+                value: settings('username', ''),
+                type: 'text',
+                helpText: 'The username used to connect to the Kepware OPC UA server'
             },
-            updateRate: {
-                value: settings('updateRate'),
-                type: 'number',
-                default: 100,
-                helpText: 'How many times per second to stream data into this server from the IoT Gateway.'
+            password: {
+                value: settings('password', ''),
+                type: 'text',
+                helpText: 'The password used to connect to the Kepware OPC UA server'
             },
             name: {
-                value: settings('name'),
+                value: settings('name', 'kepware'),
                 type: 'text',
-                helpText: 'The name of the Reality Object where nodes for each tag will be created.'
-            },
-            frameName: {
-                value: settings('frameName'),
-                type: 'text',
-                helpText: 'The name of the frame on that object where nodes will be added.'
-            },
-            tagsInfo: settings('tagsInfo')
+                helpText: 'The prefix of the Reality Object where nodes for each tag will be created.'
+            }
         };
 
         if (settings('enabled')) {
-            kepware1 = new Kepware(settings('ip'), settings('name'),  settings('port'),  settings('updateRate'), settings('tagsInfo'));
-            kepware1.setup();
+            console.log('Setting up Kepware interface');
+            kepware = new Kepware(settings('discoveryUrl', ''), {userName:settings('username', ''), password:settings('password', '')}, onItemAdd, onItemUpdate, onItemRemove);
+            kepware.connect();
+            server.enableDeveloperUI(true);
         }
     }
-
-    /**
-     * @param {string} kepwareServerIP
-     * @param {string} kepwareServerName
-     * @param {number} kepwareServerRequestInterval
-     * @param {Object} kepwareServerTagsInfo
-     */
-    function Kepware (kepwareServerIP, kepwareServerName, kepwareServerPort, kepwareServerRequestInterval, kepwareServerTagsInfo) { // eslint-disable-line no-inner-declarations
-        this.KepwareData = function() {
-            this.name = '';
-            this.id = '';
-            this.data = {
-                'id': '',
-                's': true,
-                'r': '',
-                'v': 0,
-                't': 0,
-                'min': 10000,
-                'max': 0,
-                'value': 0
-            };
-            this.dataOld = {
-                'id': '',
-                's': true,
-                'r': '',
-                'v': 0,
-                't': 0,
-                'min': 10000,
-                'max': 0,
-                'value': 0
-            };
-            this.enabled = true;
-        };
-        this.kepwareInterfaces = {};
-        server.enableDeveloperUI(true);
-        this.kepwareAddress = 'http://' + kepwareServerIP + ':' + kepwareServerPort + '/iotgateway/';
-
-        console.log('tags info saved in settings.json: ', kepwareServerTagsInfo);
-
-        /**
-         * Browse the IoT gateway and create nodes for each found tag. Also starts an update interval.
-         */
-        this.setup = function () {
-
-            this.thisID = {};
-            fetch(this.kepwareAddress + 'browse').then(res => {
-                return res.json();
-            }).then(data => {
-                for (var i = 0; i < data.browseResults.length; i++) {
-                    this.thisID = data.browseResults[i].id;
-                    this.kepwareInterfaces[this.thisID] = new this.KepwareData();
-                    this.kepwareInterfaces[this.thisID].id = data.browseResults[i].id;
-                    this.kepwareInterfaces[this.thisID].name = this.thisID.substr(this.thisID.lastIndexOf('.') + 1);
-
-                    console.log(kepwareServerName + '_' + this.kepwareInterfaces[this.thisID].name);
-
-                    // enabled by default, unless there is a specific entry in the settings.tagsInfo saying it is disabled
-                    this.kepwareInterfaces[this.thisID].enabled = typeof kepwareServerTagsInfo[this.thisID] === 'undefined' || typeof kepwareServerTagsInfo[this.thisID].enabled === 'undefined' || kepwareServerTagsInfo[this.thisID].enabled;
-
-                    // TODO: better frame naming configuration instead of just appending a '1' to the end of the object name
-                    if (this.kepwareInterfaces[this.thisID].enabled) {
-                        server.addNode(kepwareServerName, kepwareServerName + '1', this.kepwareInterfaces[this.thisID].name, 'node');
-                        this.setReadList(kepwareServerName, kepwareServerName + '1', this.thisID, this.kepwareInterfaces[this.thisID].name, this.kepwareInterfaces);
-                    } else {
-                        // remove node instead of adding if settings.tagsInfo is disabled for this node
-                        server.removeNode(kepwareServerName, kepwareServerName + '1', this.kepwareInterfaces[this.thisID].name);
-                    }
-
-                }
-
-                server.pushUpdatesToDevices(kepwareServerName);
-
-                this.interval = setInterval(this.start, kepwareServerRequestInterval);
-
-            }).catch(err => {
-                this.error(err);
-            });
-        }.bind(this);
-
-        /**
-         * When new data arrives at the node from a linked node, write the result to the kepware device using the IoT gateway.
-         */
-        this.setReadList = function(object, frame, node, name, kepwareInterfaces) {
-
-            server.addReadListener(object, frame, name, function (data) {
-
-                kepwareInterfaces[node].data.value = data.value;
-
-                const options = {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify([{id: node, v: kepwareInterfaces[node].data.value}]),
-                };
-
-                fetch(this.kepwareAddress + 'write', options).then(() => {
-                }).catch(err => {
-                    this.error(err);
-                });
-
-            }.bind(this));
-
-        }.bind(this);
-
-        /**
-         * The update interval that gets called many times per second (defined by settings('updateRate'))
-         * Reads all tags at once from the kepware device.
-         */
-        this.start = function () {
-
-            var argstring = '?';
-            for (var key in this.kepwareInterfaces) {
-                argstring += 'ids=' + key + '&';
-            }
-
-            fetch(this.kepwareAddress + 'read' + argstring).then(res => {
-                return res.json();
-            }).then(data => {
-                // parsed response body as js object
-
-                for (var i = 0; i < data.readResults.length; i++) {
-                    var thisID = data.readResults[i].id;
-
-                    if (this.kepwareInterfaces[thisID] && !this.kepwareInterfaces[thisID].enabled) {
-                        continue; // don't try to update nodes that are disabled (they don't exist!)
-                    }
-
-                    this.kepwareInterfaces[thisID].data.s = data.readResults[i].s;
-                    this.kepwareInterfaces[thisID].data.r = data.readResults[i].r;
-                    this.kepwareInterfaces[thisID].data.v = data.readResults[i].v;
-                    this.kepwareInterfaces[thisID].data.t = data.readResults[i].t;
-
-                    if (typeof this.kepwareInterfaces[thisID].data.v === 'boolean' ) { // converts boolean to 0 or 1 because nodes can only handle numbers
-                        this.kepwareInterfaces[thisID].data.v = this.kepwareInterfaces[thisID].data.v ? 1 : 0;
-                    }
-                    if (isNaN(this.kepwareInterfaces[thisID].data.v)) {
-                        console.warn(thisID + ' kepware tag value isNaN ' + this.kepwareInterfaces[thisID].data.v);
-                        this.kepwareInterfaces[thisID].data.v = 0; // uses 0 as default node value if NaN
-                    }
-
-                    let definedMin = (typeof kepwareServerTagsInfo[thisID] !== 'undefined') ? kepwareServerTagsInfo[thisID].min : undefined;
-                    let definedMax = (typeof kepwareServerTagsInfo[thisID] !== 'undefined') ? kepwareServerTagsInfo[thisID].max : undefined;
-                    let definedUnit = (typeof kepwareServerTagsInfo[thisID] !== 'undefined') ? kepwareServerTagsInfo[thisID].unit : undefined;
-
-                    if (typeof definedMax === 'undefined') {
-                        // continuously adjusts min and max based on values it's seen so far
-                        this.kepwareInterfaces[thisID].data.max = Math.max(1, Math.max(this.kepwareInterfaces[thisID].data.v, this.kepwareInterfaces[thisID].data.max));
-                    } else {
-                        this.kepwareInterfaces[thisID].data.max = definedMax;
-                    }
-
-                    if (typeof definedMin === 'undefined') {
-                        // continuously adjusts min and max based on values it's seen so far
-                        this.kepwareInterfaces[thisID].data.min = Math.min(0, Math.min(this.kepwareInterfaces[thisID].data.v, this.kepwareInterfaces[thisID].data.min));
-                    } else {
-                        this.kepwareInterfaces[thisID].data.min = definedMin;
-                    }
-
-                    // clip readings to their [min - max] range, and then normalize them to the range of [0 - 1]
-                    if (this.kepwareInterfaces[thisID].data.v < this.kepwareInterfaces[thisID].data.min) {
-                        this.kepwareInterfaces[thisID].data.v = this.kepwareInterfaces[thisID].data.min;
-                    }
-                    if (this.kepwareInterfaces[thisID].data.v > this.kepwareInterfaces[thisID].data.max) {
-                        this.kepwareInterfaces[thisID].data.v = this.kepwareInterfaces[thisID].data.max;
-                    }
-                    this.kepwareInterfaces[thisID].data.value = Math.round(server.map(this.kepwareInterfaces[thisID].data.v, this.kepwareInterfaces[thisID].data.min, this.kepwareInterfaces[thisID].data.max, 0, 1) * 1000) / 1000;
-
-                    // if the new value is different than the previous value, write to the node -> propagate value to rest of the system
-                    if (this.kepwareInterfaces[thisID].name && (this.kepwareInterfaces[thisID].dataOld.value !== this.kepwareInterfaces[thisID].data.value)) {
-
-                        // write the normalized value to the server
-                        server.write(kepwareServerName,
-                            kepwareServerName + '1', // TODO: make frame name configurable instead of just kepwareBox -> kepwareBox1
-                            this.kepwareInterfaces[thisID].name,
-                            this.kepwareInterfaces[thisID].data.value,
-                            'f', // floating point
-                            definedUnit || this.kepwareInterfaces[thisID].name,
-                            this.kepwareInterfaces[thisID].data.min,
-                            this.kepwareInterfaces[thisID].data.max);
-
-                    }
-
-                    this.kepwareInterfaces[thisID].dataOld.value = this.kepwareInterfaces[thisID].data.value;
-                }
-
-            }).catch(err => {
-                this.error(err);
-            });
-
-        }.bind(this);
-
-        /**
-         * If there's ever an error with connecting to the IoT gateway, print debug information.
-         */
-        this.error = function(_err) {
-            //  console.error('kepware error', err); // todo err just outputs a gigantic json object. Needs some more specifics.
-            console.error('cant find kepware server: \033[33m' + kepwareServerName + '\033[0m with the IP: \033[33m' + kepwareServerIP + '\033[0m');
-        };
-    }
-
-    setup();
 }
