080855db84e9ed005a89dfd49bd0fadd6922f1a6
James Hobin <jhobin@ptc.com>
2023-02-22 12:24:47 -0500

    Add region card display; Add recording button

--- tools/spatialAnalytics/index.js
+++ tools/spatialAnalytics/index.js
@@ -2,25 +2,70 @@
 
 let spatialInterface;
 
-let startTime = -1; // 1675441836335 + 20; // Date.now();
-let endTime = -1; // 1675443006371 - 30; // -1;
+let startTime = Date.now(); // 1675809876408 - 20
+let endTime = -1; // 1675809963335 + 3 * 60 * 60 * 1000;
 
 if (!spatialInterface) {
     spatialInterface = new SpatialInterface();
 }
 
-const launchButton = document.querySelector('#launchButton');
+const launchButton = document.getElementById('launchButton');
 launchButton.addEventListener('pointerup', function () {
     envelope.open();
 }, false);
 const envelopeContainer = document.querySelector('#envelopeContainer');
 const envelope = new Envelope(spatialInterface, [], envelopeContainer, launchButton, false, false);
-envelope.onOpen(() => {
-    if (startTime < 0) {
-        startTime = Date.now();
-        writePublicData();
-    }
+const recordingIcon = document.querySelector('.recordingIcon');
 
+const RecordingState = {
+    empty: 'empty',
+    recording: 'recording',
+    done: 'done',
+};
+let recordingState = RecordingState.empty;
+
+function setRecordingState(newState) {
+    recordingState = newState;
+    switch (recordingState) {
+    case RecordingState.empty:
+        recordingIcon.src = 'sprites/empty.png';
+        break;
+    case RecordingState.recording:
+        recordingIcon.src = 'sprites/recording.png';
+        break;
+    case RecordingState.done:
+        recordingIcon.style.display = 'none';
+        break;
+    }
+}
+
+recordingIcon.addEventListener('pointerup', function() {
+    console.log('recordingIcon pointerup', recordingState);
+    switch (recordingState) {
+    case RecordingState.empty:
+        setRecordingState(RecordingState.recording);
+        startTime = Date.now();
+        spatialInterface.analyticsSetDisplayRegion({
+            startTime,
+            endTime,
+        });
+        writePublicData();
+        break;
+    case RecordingState.recording:
+        setRecordingState(RecordingState.done);
+        endTime = Date.now();
+        spatialInterface.analyticsSetDisplayRegion({
+            startTime,
+            endTime,
+        });
+        writePublicData();
+        break;
+    case RecordingState.done:
+        break;
+    }
+});
+
+envelope.onOpen(() => {
     spatialInterface.analyticsAdd();
     spatialInterface.analyticsSetDisplayRegion({
         startTime,
@@ -29,7 +74,7 @@ envelope.onOpen(() => {
 });
 envelope.onClose(() => {
     spatialInterface.analyticsRemove();
-    if (endTime < 0) {
+    if (recordingState === RecordingState.recording) {
         endTime = Date.now();
         writePublicData();
     }
@@ -54,6 +99,40 @@ spatialInterface.onSpatialInterfaceLoaded(function() {
         if (status && status.hasOwnProperty('startTime')) {
             startTime = status.startTime;
             endTime = status.endTime;
+            if (startTime < 0) {
+                setRecordingState(RecordingState.empty);
+            } else if (endTime < 0) {
+                setRecordingState(RecordingState.recording);
+            } else {
+                setRecordingState(RecordingState.done);
+            }
+            if (status.summary) {
+                const container = document.createElement('div');
+                container.id = 'summaryContainer';
+                // TODO if the socket io connection is compromised then this is
+                // compromised too
+                container.innerHTML = status.summary;
+                launchButton.appendChild(container);
+                launchButton.style.width = '2400px';
+                launchButton.style.height = '1600px';
+                spatialInterface.changeFrameSize(2400, 1600);
+                const card = container.querySelector('.analytics-region-card');
+                card.setAttribute('style', '');
+                // card.classList.add('minimized');
+                // card.addEventListener('pointermove', () => { // should be `over`
+                //     card.classList.remove('minimized');
+                // });
+                // card.addEventListener('pointerout', () => {
+                //     card.classList.add('minimized');
+                // });
+                let pin = container.querySelector('.analytics-region-card-pin');
+                pin.parentNode.removeChild(pin);
+                let enter = container.querySelector('.analytics-region-card-enter');
+                enter.parentNode.removeChild(enter);
+
+                const launchIcon = document.getElementById('launchIcon');
+                launchIcon.parentNode.removeChild(launchIcon);
+            }
         }
     });
 });
