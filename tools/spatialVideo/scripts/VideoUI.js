e3d5352b20785b256b86e87d1f5c94eeeaa583ca
ktheo-ptc <ktheodosiou@ptc.com>
2023-05-04 18:07:58 -0400

    added background to video ui

--- tools/spatialVideo/scripts/VideoUI.js
+++ tools/spatialVideo/scripts/VideoUI.js
@@ -13,6 +13,7 @@ class VideoUI {
         this.parentElement = parentElement;
         this.callbacks = callbacks;
         this.icons = ['empty', 'emptyBlocked', 'paused', 'recording', 'playing', 'loading', 'saving', 'waitingForUser'].map(iconName => {
+            /* <---- all icons get added to dom here ---->  */
             const imageElement = document.createElement('img');
             if (iconName === 'saving') {
                 imageElement.src = 'sprites/saving0.png';
@@ -22,6 +23,7 @@ class VideoUI {
             imageElement.iconName = iconName;
             this.parentElement.appendChild(imageElement);
             imageElement.hidden = true;
+            /* <---- ------------------------------ ---->  */
             imageElement.addEventListener('pointerup', e => {
                 if (e.button === 0) {
                     this.callbacks.onButtonPress(this);
@@ -35,19 +37,23 @@ class VideoUI {
 
         this.spriteAnimationStartTime = Date.now();
         this.savingSrcs = [0, 1, 2, 3].map(index => `sprites/saving${index}.png`);
+        //icons are animated together here
         this.animateIcons();
 
         this.setState(VideoUIStates.EMPTY);
     }
-
+    
     animateIcons() {
         const elapsedTime = Date.now() - this.spriteAnimationStartTime;
         const modulo = Math.floor((elapsedTime / 1000 * 2) % 4); // Change saving animation frame twice per second
+        //wait to mount before beginning
         this.icons.getByName('saving').src = this.savingSrcs[modulo];
+        //wait to mount before beginning
         this.icons.getByName('loading').style.transform = `rotate(${elapsedTime / 1000 * 2 * Math.PI / 4}rad)`; // One rotation per four seconds
         window.requestAnimationFrame(() => this.animateIcons());
     }
 
+    //this should mount and unmount the icons
     setIconByName(iconName) {
         this.icons.forEach(icon => icon.hidden = true);
         this.icons.getByName(iconName).hidden = false;
