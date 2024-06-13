e11a7eca03457302e7346d5afb6297867edb1f09
James Hobin <jhobin@ptc.com>
2024-06-13 12:56:53 -0400

    Add mostly working polygonal spatial sensor

---
+++ tools/spatialSensorPolygon/winding.js
@@ -0,0 +1,19 @@
+export function isPointInsideWalls(point, vs) {
+    // ray-casting algorithm based on
+    // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+    // and in turn https://observablehq.com/@tmcw/understanding-point-in-polygon
+
+    const {x, y} = point;
+
+    var inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        var xi = vs[i].x, yi = vs[i].y;
+        var xj = vs[j].x, yj = vs[j].y;
+
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+
+    return inside;
+}
